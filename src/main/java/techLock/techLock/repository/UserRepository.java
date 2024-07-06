41086a5212d82dfc9c64693dfa0013b0dac1aa87
nam <yhnham@gmail.com>
2024-07-06 21:08:27 +0900

    ãlogin modified

--- src/main/java/techLock/techLock/repository/UserRepository.java
+++ src/main/java/techLock/techLock/repository/UserRepository.java
@@ -3,7 +3,9 @@ package techLock.techLock.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import techLock.techLock.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
